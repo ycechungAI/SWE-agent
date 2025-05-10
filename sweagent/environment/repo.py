eb17253e1442b56f589d81b4373c2aca27921b2b
Kilian Lieret <kilian.lieret@posteo.de>
2025-05-10 19:38:18 +0200

    Fix(repo): Ensure absolute path for copy repo (#1116)

--- sweagent/environment/repo.py
+++ sweagent/environment/repo.py
@@ -160,8 +160,8 @@ class GithubRepoConfig(BaseModel):
                 Command(
                     command=" && ".join(
                         (
-                            f"mkdir {self.repo_name}",
-                            f"cd {self.repo_name}",
+                            f"mkdir /{self.repo_name}",
+                            f"cd /{self.repo_name}",
                             "git init",
                             f"git remote add origin {url}",
                             f"git fetch --depth 1 origin {base_commit}",
