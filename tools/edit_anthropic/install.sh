a42ec4fe973a3d2113ba867e98c52b39071cc9d2
Kilian Lieret <kilian.lieret@posteo.de>
2025-05-31 14:10:57 -0400

    Fix: Ignore missing treesitter for very old python (#1180)
    Closes #1179

--- tools/edit_anthropic/install.sh
+++ tools/edit_anthropic/install.sh
@@ -1,2 +1,3 @@
-pip install 'tree-sitter==0.21.3'
-pip install 'tree-sitter-languages'
+# Ignore failures, see https://github.com/SWE-agent/SWE-agent/issues/1179
+pip install 'tree-sitter==0.21.3' || true
+pip install 'tree-sitter-languages' || true
